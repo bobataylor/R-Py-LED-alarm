f78d0d4ef519bfc414fefe1313b9b44446d7fa48
bobataylor <cam@rtastudio.com>
2018-02-13 04:22:50 +0000

    small changes to account for testing and changes in other files

--- run_me.py
+++ run_me.py
@@ -1,12 +1,15 @@
 import alarm, leds, g_socket
 from colors import colors
 import RPi.GPIO as GPIO
-import socket, threading
+import socket, threading, sys
 
 def main():
     HOUR, MIN, TZ = 6, 50, 5
     R, G, B = 17, 27, 22
     PORT = 7777
+    TEST = sys.argv[1]
+    fade_time = 10.0
+    if TEST: time = .1
 
     try:
         #TODO thread the alarm and the socket with the shared led_strip object
@@ -14,8 +17,8 @@ def main():
         led_strip = leds.leds(R, G, B, 300)
         
         while True:
-            if alarm.check_time(HOUR, MIN, TZ):
-                led_strip.fade_in(colors.orange, 10)
+            if alarm.check_time(HOUR, MIN, TZ) or TEST:
+                led_strip.fade_in(colors.dark_orange, time)
     except KeyboardInterrupt:
         pass
     finally:
