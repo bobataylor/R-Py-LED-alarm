81b61c4fd977b8bd86c55a8f1b13beef66cae930
bobataylor <cam@rtastudio.com>
2018-02-13 16:31:49 +0000

    Added a brightness value and channel to the leds class. Added set_brightness method and implemented it into other methods

--- leds.py
+++ leds.py
@@ -27,23 +27,27 @@ Each color is created as its own channel object.
 The single container approach is to simplify outside access to the leds by abstracting away the various color channels and making the leds appread as one object.
 '''
 class leds:
-    def __init__(self, R, G ,B, freq):
+    def __init__(self, R, G ,B, P, freq):
         self.red   = channel(R, freq, 0.0)
         self.green = channel(G, freq, 0.0)
         self.blue  = channel(B, freq, 0.0)
-        self.channels = [self.red, self.green, self.blue]
+        self.power = channel(P, freq, 0.0)
+        self.channels = [self.red, self.green, self.blue, self.power]
 
         self.color = [0.0, 0.0, 0.0]
+        self.brightness = 0.0
 
     def on(self):
         self.red.on()
         self.green.on()
         self.blue.on()
+        self.power.on()
 
     def off(self):
         self.red.off()
         self.blue.off()
         self.green.off()
+        self.power.off()
 
     def set_color(self, color):
         '''
@@ -56,14 +60,22 @@ class leds:
         '''
         self.color = color
         for i in range(0,3):
-            if color[i] > 0:
-                duty_cycle = (float(color[i])/255.0) * 100.0
-            else:
-                duty_cycle = 0
+            if color[i] < 0.0:
+                color[i] = 0.0
+            elif color[i] > 100.0:
+                color[i] = 100.0
+            duty_cycle = (float(color[i])/255.0) * 100.0
             self.channels[i].set_duty(duty_cycle)
 
+    def set_brightness(self, brightness):
+        if brightness > 100.0:
+            brightness = 100.0
+        elif brightness < 0.0:
+            brightness = 0.0
+        self.power.set_duty(brightness)
+        self.brightness = brightness
 
-    def fade_in(self, color, speed):
+    def fade_in(self, color, brightness, speed):
         wait = 0.1
         #Determine the step size by which to change each channels duty cycle.
         #Should work regardless of current values
@@ -74,7 +86,8 @@ class leds:
         step = [
                 (color[0] - self.color[0])/num_steps,
                 (color[1] - self.color[1])/num_steps,
-                (color[2] - self.color[2])/num_steps]
+                (color[2] - self.color[2])/num_steps,
+                (brightness - self.brightness)/num_steps]
 
         #finally, set the new step color
         for i in range(0, int(num_steps)):
@@ -82,7 +95,9 @@ class leds:
                     self.color[0] + step[0],
                     self.color[1] + step[1],
                     self.color[2] + step[2]]
+            step_brightness = self.brightness + step[3]
             self.set_color(step_color)
+            self.set_brightness(step_brightness)
             #not needed if our math is good
             if color == self.color:
                 break # we have reached the final color, exit this function
