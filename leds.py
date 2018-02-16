9ae563e468bd57362bd69e2ab068c2c3e95beb54
bobataylor <cam@rtastudio.com>
2018-02-16 20:22:38 +0000

    Removed brightness. Began change to gamma correction.

--- leds.py
+++ leds.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+from colors import colors
 
 class channel:
     def __init__(self, pin, freq, duty):
@@ -15,7 +16,7 @@ class channel:
     def on(self):
         GPIO.setup(self.pin, GPIO.OUT, initial=GPIO.LOW)
         self.pwm = GPIO.PWM(self.pin, self.frequency)
-        self.pwm.start(0)
+        self.pwm.start(self.duty_cycle)
     
     def off(self):
         self.pwm.stop()
@@ -27,27 +28,23 @@ Each color is created as its own channel object.
 The single container approach is to simplify outside access to the leds by abstracting away the various color channels and making the leds appread as one object.
 '''
 class leds:
-    def __init__(self, R, G ,B, P, freq):
-        self.red   = channel(R, freq, 0.0)
-        self.green = channel(G, freq, 0.0)
-        self.blue  = channel(B, freq, 0.0)
-        self.power = channel(P, freq, 0.0)
+    def __init__(self, R, G ,B, freq):
+        self.red   = channel(R, freq, 0)
+        self.green = channel(G, freq, 0)
+        self.blue  = channel(B, freq, 0)
         self.channels = [self.red, self.green, self.blue, self.power]
 
-        self.color = [0.0, 0.0, 0.0]
-        self.brightness = 0.0
+        self.color = [0, 0, 0]  
 
     def on(self):
         self.red.on()
         self.green.on()
         self.blue.on()
-        self.power.on()
 
     def off(self):
         self.red.off()
         self.blue.off()
         self.green.off()
-        self.power.off()
 
     def set_color(self, color):
         '''
@@ -60,34 +57,27 @@ class leds:
         '''
         self.color = color
         for i in range(0,3):
-            if color[i] < 0.0:
-                color[i] = 0.0
-            elif color[i] > 100.0:
-                color[i] = 100.0
-            duty_cycle = (float(color[i])/255.0) * 100.0
+            if color[i] < 0:
+                color[i] = 0
+            elif color[i] > 100:
+                color[i] = 100
+            duty_cycle = (float(colors.gamma[color[i]])/255.0) * 100.0
             self.channels[i].set_duty(duty_cycle)
 
-    def set_brightness(self, brightness):
-        if brightness > 100.0:
-            brightness = 100.0
-        elif brightness < 0.0:
-            brightness = 0.0
-        self.power.set_duty(brightness)
-        self.brightness = brightness
 
-    def fade_in(self, color, brightness, speed):
+    def fade_in(self, color, speed):
         wait = 0.1
+        color = [colors.gamma[color[0]], colors.gamma[color[1]], colors.gamma[colors[2]]]
         #Determine the step size by which to change each channels duty cycle.
         #Should work regardless of current values
 
         #first let's get the current RGB values of the strip
         #Then lets find each channels step size 
-        num_steps = (speed * 60.0 * (1.0/wait))
+        num_steps = (float(speed) * 60.0 * (1.0/wait))
         step = [
-                (color[0] - self.color[0])/num_steps,
-                (color[1] - self.color[1])/num_steps,
-                (color[2] - self.color[2])/num_steps,
-                (brightness - self.brightness)/num_steps]
+                float(color[0] - self.color[0])/num_steps,
+                float(color[1] - self.color[1])/num_steps,
+                float(color[2] - self.color[2])/num_steps]
 
         #finally, set the new step color
         for i in range(0, int(num_steps)):
@@ -95,10 +85,7 @@ class leds:
                     self.color[0] + step[0],
                     self.color[1] + step[1],
                     self.color[2] + step[2]]
-            step_brightness = self.brightness + step[3]
+    
             self.set_color(step_color)
-            self.set_brightness(step_brightness)
-            #not needed if our math is good
-            if color == self.color:
-                break # we have reached the final color, exit this function
+           
             time.sleep(wait)
--- run_me.py
+++ run_me.py
@@ -5,7 +5,7 @@ import socket, threading, sys, time
 
 def main():
     HOUR, MIN, TZ = 6, 45, 5
-    R, G, B, P = 17, 27, 22, 7 
+    R, G, B, P = 17, 27, 22 
     PORT = 7777
     TEST = sys.argv[1]
     fade_time = 10.0
@@ -15,14 +15,14 @@ def main():
     try:
         #TODO thread the alarm and the socket with the shared led_strip object
         GPIO.setmode(GPIO.BCM)
-        led_strip = leds.leds(R, G, B, P, 300)
-        
+        led_strip = leds.leds(R, G, B, 300)
         while True:
             if alarm.check_time(HOUR, MIN, TZ) or TEST == '1':
+                print('wake up')
                 led_strip.on()
-                led_strip.fade_in(colors.orange, 30.0, fade_time)
-                #led_strip.set_color(colors.dark_orange, 30.0)
-                time.sleep(10*60)
+                led_strip.fade_in(colors.dark_orange, fade_time)
+                #led_strip.set_color(colors.dark_orange)
+                time.sleep(20*60)
                 les_strip.off()
             time.sleep(30)
     except KeyboardInterrupt:
