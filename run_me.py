d6ab603a302c5d074f28a3216d9699773b5f13cd
bobataylor <cam@rtastudio.com>
2018-03-06 02:51:24 +0000

    Added / fixed the ability to turn the lights off

--- run_me.py
+++ run_me.py
@@ -22,8 +22,11 @@ def assistant(led_strip):
         index = color.find("&color=")
         color = color[index+7:].lower()
         print("you said {}".format(color))
-        led_strip.on()
-        led_strip.set_color(color)
+        if 'off' in color:
+            led_strip.off()
+        else:
+            led_strip.on()
+            led_strip.set_color(color)
 
 
 def alarmclock(led_strip, HOUR, MIN, TZ, TEST):
@@ -31,8 +34,6 @@ def alarmclock(led_strip, HOUR, MIN, TZ, TEST):
     while True:
         if alarm.check_time(HOUR, MIN, TZ) or TEST == '1':
             print('wake up')
-            #Protect critical section section
-            #Prevent led_strip object from experiencing race conditions
             led_strip.on()
             led_strip.fade_in(colors.dark_orange, fade_time)
             time.sleep(20*60)
@@ -53,7 +54,7 @@ def main():
         #TODO thread the alarm and the socket with the shared led_strip object
         GPIO.setmode(GPIO.BCM)
         led_strip = leds.leds(R, G, B, 300)
-        
+        led_strip.on()
         tid1 = threading.Thread(target=assistant, args=[led_strip])
         tid2 = threading.Thread(target=alarmclock, args=[led_strip, HOUR, MIN, TZ, TEST])
         tid1.daemon = True
